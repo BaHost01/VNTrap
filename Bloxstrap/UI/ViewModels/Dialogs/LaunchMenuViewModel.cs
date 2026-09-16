28bfab53dd8523b57b640d5a4b74c12fb9be69bf
Redacted <guilhermeagenteguilherme@gmail.com>
2026-09-16 12:23:38 -0300

    feat: add multi-instance launch action

--- Bloxstrap/UI/ViewModels/Dialogs/LaunchMenuViewModel.cs
+++ Bloxstrap/UI/ViewModels/Dialogs/LaunchMenuViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.Input;
 
@@ -13,11 +14,9 @@ namespace Bloxstrap.UI.ViewModels.Installer
         public Visibility RobloxStudioOptionVisibility => App.IsStudioVisible ? Visibility.Visible : Visibility.Collapsed;
 
         public ICommand LaunchSettingsCommand => new RelayCommand(LaunchSettings);
-
         public ICommand LaunchRobloxCommand => new RelayCommand(LaunchRoblox);
-
+        public ICommand LaunchMultiRobloxCommand => new RelayCommand(LaunchMultiRoblox);
         public ICommand LaunchRobloxStudioCommand => new RelayCommand(LaunchRobloxStudio);
-
         public ICommand LaunchAboutCommand => new RelayCommand(LaunchAbout);
 
         public event EventHandler<NextAction>? CloseWindowRequest;
@@ -25,6 +24,27 @@ namespace Bloxstrap.UI.ViewModels.Installer
         private void LaunchSettings() => CloseWindowRequest?.Invoke(this, NextAction.LaunchSettings);
 
         private void LaunchRoblox() => CloseWindowRequest?.Invoke(this, NextAction.LaunchRoblox);
+
+        private void LaunchMultiRoblox()
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = Paths.Application,
+                    Arguments = "-player -multi 2 -performance",
+                    WorkingDirectory = Paths.Base,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                App.Logger.WriteException("LaunchMenuViewModel::LaunchMultiRoblox", ex);
+                Frontend.ShowMessageBox(ex.Message, MessageBoxImage.Error);
+            }
+
+            CloseWindowRequest?.Invoke(this, NextAction.Terminate);
+        }
 
         private void LaunchRobloxStudio() => CloseWindowRequest?.Invoke(this, NextAction.LaunchRobloxStudio);
 
