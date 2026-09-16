2f513d23d76e860481502ff20138b94b334d22ee
Redacted <guilhermeagenteguilherme@gmail.com>
2026-09-16 12:23:19 -0300

    ui: remove random launch title mutation

--- Bloxstrap/UI/Elements/Dialogs/LaunchMenuDialog.xaml.cs
+++ Bloxstrap/UI/Elements/Dialogs/LaunchMenuDialog.xaml.cs
@@ -1,25 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using Bloxstrap.UI.ViewModels.Dialogs;
 using Bloxstrap.UI.ViewModels.Installer;
 using Wpf.Ui.Mvvm.Interfaces;
 
 namespace Bloxstrap.UI.Elements.Dialogs
 {
-    /// <summary>
-    /// Interaction logic for LaunchMenuDialog.xaml
-    /// </summary>
     public partial class LaunchMenuDialog
     {
         public NextAction CloseAction = NextAction.Terminate;
@@ -34,13 +19,7 @@ namespace Bloxstrap.UI.Elements.Dialogs
             };
 
             DataContext = viewModel;
-
             InitializeComponent();
-            Random Chance = new();
-            if (Chance.Next(0, 10000) == 1)
-            {
-                LaunchTitle.Text = "Fishtrap";
-            }
         }
     }
 }
